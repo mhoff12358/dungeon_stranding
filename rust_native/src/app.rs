5a47d8ed8ccf4adfed31286100a315264a538c68
mhoff <mhoff12358@gmail.com>
2023-08-15 23:17:48 -0500

    Initial ds code integration.

--- rust_native/src/app.rs
+++ rust_native/src/app.rs
@@ -4,8 +4,10 @@ use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Node)]
-struct App {
+pub struct App {
     app: ds_lib::app::App,
+
+    name: GodotString,
 
     #[base]
     base: Base<Node>,
@@ -20,6 +22,7 @@ impl NodeVirtual for App {
 
         Self {
             app: ds_lib::app::App::new(&args),
+            name: "AN APP!".into(),
             base,
         }
     }
@@ -27,4 +30,28 @@ impl NodeVirtual for App {
     fn to_string(&self) -> godot::builtin::GodotString {
         self.base.to_string().into()
     }
+}
+
+impl App {
+    pub fn find_app(node: &Gd<Node>) -> Gd<App> {
+        let tree = node.get_tree().unwrap();
+        let scene_tree = tree.get_current_scene().unwrap();
+        return look_for_app(&scene_tree).unwrap();
+    }
+
+    pub fn get_name(&self) -> &GodotString {
+        return &self.name;
+    }
+}
+
+fn look_for_app(node: &Gd<Node>) -> Option<Gd<App>> {
+    if node.is_class(App::class_name().to_godot_string()) {
+        return Some(node.share().cast());
+    }
+    for child_index in 0..node.get_child_count() {
+        if let Some(look_children) = look_for_app(node.get_child(child_index).as_ref().unwrap()) {
+            return Some(look_children);
+        }
+    }
+    return None;
 }
--- rust_native/src/game_state.rs
+++ rust_native/src/game_state.rs
@@ -1,9 +1,14 @@
+use std::ops::Deref;
+
+use crate::app::App;
 use godot::engine::{Control, ControlVirtual};
 use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Control)]
-struct GameState {
+pub struct GameState {
+    app: Option<Gd<App>>,
+
     #[base]
     base: Base<Control>,
 }
@@ -13,7 +18,15 @@ impl ControlVirtual for GameState {
     fn init(base: godot::obj::Base<Self::Base>) -> Self {
         godot::engine::utilities::print("Creating GameState".to_variant(), &[]);
 
-        Self { base }
+        Self { base, app: None }
+    }
+
+    fn ready(&mut self) {
+        self.app = Some(App::find_app(&self.base.share().upcast()));
+        godot::engine::utilities::print(
+            "GameState found app: ".to_variant(),
+            &[self.app.as_ref().unwrap().bind().get_name().to_variant()],
+        );
     }
 
     fn to_string(&self) -> godot::builtin::GodotString {
